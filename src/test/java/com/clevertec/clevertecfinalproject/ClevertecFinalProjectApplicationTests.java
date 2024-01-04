5a8bd0be26b3e0fc208842ac2bfb65abceed5460
Vlad Kuchuk <bboysmile2003@mail.ru>
2024-01-05 00:06:01 +0300

    Init with TODOs in Yamls & docker-compose.yml

---
+++ src/test/java/com/clevertec/clevertecfinalproject/ClevertecFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clevertec.clevertecfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClevertecFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
